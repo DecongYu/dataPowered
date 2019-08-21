c67c70b8b90b8e8130f74aced6ec0af528e1018d
Chris Yu <49385992+chrisyucanada@users.noreply.github.com>
2019-08-20 22:19:23 -0600

    database init creation

---
+++ schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS well;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  email TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE well (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  entered TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  well_id TEXT NOT NULL,
+  on_prd_date DATE NOT NULL,
+  tvd REAL NOT NULL,
+  frac_len REAL NOT NULL,
+  cum_frac_propp REAL NOT NULL,
+  bh_long REAL NOT NULL,
+  bh_lat REAL NOT NULL,
+  latrl_len REAL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
